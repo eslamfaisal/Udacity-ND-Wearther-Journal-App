b2fc73a571bb7b1f2ecd8a108bc8a6fa70b8865c
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-11-30 16:32:02 +0200

    define form variable

--- website/app.js
+++ website/app.js
@@ -5,3 +5,5 @@ const appId = 'ae684c8809f260571a8216177ef33e56';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const weatherDataForm = document.getElementById('weatherDataForm');
